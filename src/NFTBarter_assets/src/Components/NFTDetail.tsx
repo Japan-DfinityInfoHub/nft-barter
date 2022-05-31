48f3d1dee9bbfc888593f6818bce6241bca1b76a
hoosan <hoosanalis@gmail.com>
2022-05-31 20:57:19 +0900

    Add validation for token id

--- src/NFTBarter_assets/src/Components/NFTDetail.tsx
+++ src/NFTBarter_assets/src/Components/NFTDetail.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Center } from '@chakra-ui/react';
+import { decodeTokenId } from '../utils/ext';
+import { NotFound } from './NotFound';
+
+const CANISTER_ID =
+  process.env.DFX_NETWORK === 'ic' || !process.env.LOCAL_NFT_CANISTER_ID
+    ? 'REPLACE_TO_CANISTER_ID'
+    : process.env.LOCAL_NFT_CANISTER_ID;
 
 export const NFTDetail = () => {
   const params = useParams();
   const tokenId = params.tokenId;
+  if (!tokenId) {
+    return <NotFound />;
+  }
+
+  const { canisterId } = decodeTokenId(tokenId);
+
+  // So far we only accept GenerativeArtNFT canister
+  if (canisterId !== CANISTER_ID) {
+    return <NotFound />;
+  }
 
   return <Center h='80vh'>{tokenId}</Center>;
 };
--- src/NFTBarter_assets/src/utils/ext.ts
+++ src/NFTBarter_assets/src/utils/ext.ts
@@ -24,6 +24,10 @@ export const getSubAccount = (index: number): number[] => {
 };
 
 export const decodeTokenId = (tid: string): TokenProps => {
+  if (!checkIfTextIsPrincipal(tid)) {
+    return { index: 0, canisterId: tid };
+  }
+
   const array = [...Principal.fromText(tid).toUint8Array()];
   const padding = new Uint8Array(array.splice(0, 4));
   if (toHexString(padding) !== toHexString(Buffer.from('\x0Atid'))) {
@@ -32,6 +36,15 @@ export const decodeTokenId = (tid: string): TokenProps => {
     const index = numberFrom32bits(array.splice(-4));
     const canisterId = Principal.fromUint8Array(new Uint8Array(array)).toText();
     return { index, canisterId };
+  }
+};
+
+const checkIfTextIsPrincipal = (text: string): boolean => {
+  try {
+    Principal.fromText(text);
+    return true;
+  } catch (e) {
+    return false;
   }
 };
 
