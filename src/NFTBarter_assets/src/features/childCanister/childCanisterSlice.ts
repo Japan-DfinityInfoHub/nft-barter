7ff5c44dce356698c9bedc8a8f1438d1b79d9534
hoosan <hoosanalis@gmail.com>
2022-06-04 14:36:08 +0900

    Add getChildCanister and createChildCanister to childCanisterSlice

--- src/NFTBarter_assets/src/features/childCanister/childCanisterSlice.ts
+++ src/NFTBarter_assets/src/features/childCanister/childCanisterSlice.ts
@@ -1,33 +1,77 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { AuthClient } from '@dfinity/auth-client';
-import { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
 
 import { RootState, AsyncThunkConfig } from '../../app/store';
 import { createNFTBarterActor } from '../../utils/createNFTBarterActor';
-import {
-  UserProfile,
-  Result,
-  Error,
-} from '../../../../declarations/NFTBarter/NFTBarter.did';
+import { Error } from '../../../../declarations/NFTBarter/NFTBarter.did';
 
 export interface ChildCanisterState {
+  canisterIds: Principal[];
   error?: Error;
 }
 
-const initialState: ChildCanisterState = {};
+const initialState: ChildCanisterState = {
+  canisterIds: [],
+};
 
-export const create = createAsyncThunk<
+export const getChildCanisters = createAsyncThunk<
   ChildCanisterState,
   undefined,
   AsyncThunkConfig<{ error: Error }>
->('childCanisterSlice/create', async (_, { rejectWithValue }) => {
+>('childCanister/get', async (_, { rejectWithValue }) => {
   const authClient = await AuthClient.create();
 
-  if (!authClient) {
-    throw new Error('Failed to use auth client.');
+  if (!authClient || !authClient.isAuthenticated()) {
+    return rejectWithValue({
+      error: { unauthorized: 'Failed to use auth client.' },
+    });
   }
 
-  return rejectWithValue({ error: { other: '' } });
+  const identity = await authClient.getIdentity();
+  const actor = createNFTBarterActor({
+    agentOptions: { identity },
+  });
+
+  const res = await actor.getMyChildCanisters();
+  if ('ok' in res) {
+    const canisterIds = res.ok;
+    return {
+      canisterIds,
+    };
+  } else {
+    return rejectWithValue({
+      error: res.err,
+    });
+  }
+});
+
+export const createChildCanister = createAsyncThunk<
+  ChildCanisterState,
+  undefined,
+  AsyncThunkConfig<{ error: Error }>
+>('childCanister/create', async (_, { rejectWithValue }) => {
+  const authClient = await AuthClient.create();
+
+  if (!authClient || !authClient.isAuthenticated()) {
+    return rejectWithValue({
+      error: { unauthorized: 'Failed to use auth client.' },
+    });
+  }
+
+  const identity = await authClient.getIdentity();
+  const actor = createNFTBarterActor({
+    agentOptions: { identity },
+  });
+
+  const res = await actor.mintChildCanister();
+  if ('ok' in res) {
+    return { canisterIds: [res.ok] };
+  } else {
+    return rejectWithValue({
+      error: res.err,
+    });
+  }
 });
 
 export const childCanisterSlice = createSlice({
@@ -35,13 +79,26 @@ export const childCanisterSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(create.fulfilled, (state, action) => {});
-    builder.addCase(create.rejected, (state, action) => {
+    builder.addCase(getChildCanisters.fulfilled, (state, action) => {
+      state.canisterIds = action.payload?.canisterIds;
+    });
+    builder.addCase(getChildCanisters.rejected, (state, action) => {
+      state.error = action.payload?.error;
+    });
+    builder.addCase(createChildCanister.fulfilled, (state, action) => {
+      state.canisterIds = [
+        ...state.canisterIds,
+        ...action.payload?.canisterIds,
+      ];
+    });
+    builder.addCase(createChildCanister.rejected, (state, action) => {
       state.error = action.payload?.error;
     });
   },
 });
 
+export const selectCanisterIds = (state: RootState) =>
+  state.childCanister.canisterIds;
 export const selectError = (state: RootState) => state.childCanister.error;
 
 export default childCanisterSlice.reducer;
