58aeb4f7e36cb77e3b613fd3da5bd01cfb1b7739
hoosan <hoosanalis@gmail.com>
2022-06-04 14:23:55 +0900

    Add getMyChildCanisters method to main canister

--- src/declarations/NFTBarter/NFTBarter.did.d.ts
+++ src/declarations/NFTBarter/NFTBarter.did.d.ts
@@ -1,15 +1,21 @@
 import type { Principal } from '@dfinity/principal';
+export type CanisterID = Principal;
 export type Error = { 'other' : string } |
   { 'alreadyRegistered' : string } |
   { 'unauthorized' : string } |
   { 'notYetRegistered' : string };
 export interface NFTBarter {
+  'getMyChildCanisters' : () => Promise<Result_2>,
   'getMyProfile' : () => Promise<Result>,
   'isRegistered' : () => Promise<boolean>,
-  'mintChildCanister' : () => Promise<Principal>,
+  'mintChildCanister' : () => Promise<Result_1>,
   'register' : () => Promise<Result>,
 }
 export type Result = { 'ok' : UserProfile } |
+  { 'err' : Error };
+export type Result_1 = { 'ok' : CanisterID } |
+  { 'err' : Error };
+export type Result_2 = { 'ok' : Array<CanisterID> } |
   { 'err' : Error };
 export type UserProfile = { 'none' : null };
 export interface _SERVICE extends NFTBarter {}
