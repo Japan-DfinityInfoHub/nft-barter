60fa3d64742243e81a1d8a622a0d3f45371791e9
hoosan <hoosanalis@gmail.com>
2022-06-04 14:24:38 +0900

    Add test for mintChildCanister and getMyChildCanisters

---
+++ src/tests/e2e/mintChildCanister.test.ts
@@ -0,0 +1,84 @@
+import { Secp256k1KeyIdentity } from '@dfinity/identity';
+import fetch from 'isomorphic-fetch';
+import { IDL } from '@dfinity/candid';
+
+declare module '../../declarations/NFTBarter/NFTBarter.did.js' {
+  function idlFactory(): IDL.ServiceClass;
+}
+import {
+  CanisterID,
+  _SERVICE as INFTBarter,
+  idlFactory,
+} from '../../declarations/NFTBarter/NFTBarter.did.js';
+import { curriedCreateActor } from '../../NFTBarter_assets/src/utils/createActor';
+import localCanisterIds from '../../../.dfx/local/canister_ids.json';
+
+const canisterId = localCanisterIds.NFTBarter.local;
+
+// We don't import `createNFTBarterActor` from `createNFTBarterActor.ts`
+// because it gives an error in jest running on GitHub Actions.
+const createNFTBarterActor =
+  curriedCreateActor<INFTBarter>(idlFactory)(canisterId);
+
+const identityOptionOfAlice = {
+  agentOptions: {
+    identity: Secp256k1KeyIdentity.generate(),
+    fetch,
+    host: 'http://localhost:8000',
+  },
+};
+const actorOfAlice = createNFTBarterActor(identityOptionOfAlice);
+
+const identityOptionOfAnonymous = {
+  agentOptions: {
+    fetch,
+    host: 'http://localhost:8000',
+  },
+};
+const actorOfAnonymous = createNFTBarterActor(identityOptionOfAnonymous);
+
+describe('Mint child canister test', () => {
+  let childCanisterId: CanisterID;
+
+  beforeAll(async () => {
+    await actorOfAlice.register();
+  });
+
+  it('Alice can mint a child canister', async () => {
+    const res = await actorOfAlice.mintChildCanister();
+    expect(res).toStrictEqual({
+      ok: expect.anything(),
+    });
+    if ('ok' in res) {
+      childCanisterId = res.ok;
+    }
+    expect(childCanisterId._isPrincipal).toBe(true);
+  });
+
+  it('Alice can get her child caniser', async () => {
+    let res = await actorOfAlice.getMyChildCanisters();
+    expect(res).toStrictEqual({
+      ok: [childCanisterId],
+    });
+  });
+});
+
+describe('Anonymous tests', () => {
+  it('Anomymous identity can not mint any child canister.', async () => {
+    const res = await actorOfAnonymous.mintChildCanister();
+    expect(res).toEqual({
+      err: {
+        unauthorized: expect.anything(),
+      },
+    });
+  });
+
+  it('Anonymous identity never has its own child canister.', async () => {
+    const res = await actorOfAnonymous.getMyChildCanisters();
+    expect(res).toEqual({
+      err: {
+        unauthorized: expect.anything(),
+      },
+    });
+  });
+});
