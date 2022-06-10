81865615f123c4aef621a5ed56bb0537f9d5c947
hoosan <hoosanalis@gmail.com>
2022-06-10 17:58:21 +0900

    Add test for bidding

--- src/tests/e2e/exhibit.test.ts
+++ src/tests/e2e/exhibit.test.ts
@@ -2,7 +2,7 @@ import { Secp256k1KeyIdentity } from '@dfinity/identity';
 import { ActorSubclass } from '@dfinity/agent';
 import fetch from 'isomorphic-fetch';
 import { IDL } from '@dfinity/candid';
-import { jest } from '@jest/globals'
+import { jest } from '@jest/globals';
 
 declare module '../../declarations/NFTBarter/NFTBarter.did.js' {
   function idlFactory(): IDL.ServiceClass;
@@ -34,6 +34,7 @@ import {
   generateTokenIdentifier,
   principalToAccountIdentifier,
 } from '../../NFTBarter_assets/src/utils/ext';
+import { getTokenIdAndNftStatusFromAsset } from '../../NFTBarter_assets/src/utils/nft';
 import localCanisterIds from '../../../.dfx/local/canister_ids.json';
 
 const canisterId = localCanisterIds.NFTBarter.local;
@@ -43,8 +44,9 @@ const canisterId = localCanisterIds.NFTBarter.local;
 const createNFTBarterActor =
   curriedCreateActor<INFTBarter>(idlFactory)(canisterId);
 
-const createGenerativeNFTActor =
-  curriedCreateActor<IGenerativeArtNFT>(nftIdlFactory)(generativeNFTCanisterId);
+const createGenerativeNFTActor = curriedCreateActor<IGenerativeArtNFT>(
+  nftIdlFactory
+)(generativeNFTCanisterId);
 
 const createChildCanisterActorByCanisterId =
   curriedCreateActor<IChildCanister>(childIdlFactory);
@@ -61,10 +63,26 @@ const userAlice: User = {
 };
 
 const actorOfAlice = createNFTBarterActor(identityOptionOfAlice);
-const generativeNFTActorOfAlice = createGenerativeNFTActor(identityOptionOfAlice);
+const generativeNFTActorOfAlice = createGenerativeNFTActor(
+  identityOptionOfAlice
+);
+
+const identityOptionOfBob = {
+  agentOptions: {
+    identity: Secp256k1KeyIdentity.generate(),
+    fetch,
+    host: 'http://localhost:8000',
+  },
+};
+const userBob: User = {
+  principal: identityOptionOfBob.agentOptions.identity.getPrincipal(),
+};
+
+const actorOfBob = createNFTBarterActor(identityOptionOfBob);
+const generativeNFTActorOfBob = createGenerativeNFTActor(identityOptionOfBob);
 
 // beforeAll has long-running calls
-jest.setTimeout(12000);
+jest.setTimeout(60000);
 
 describe('Exhibit an NFT test', () => {
   let tokenId: TokenIdentifier;
@@ -89,8 +107,13 @@ describe('Exhibit an NFT test', () => {
     if ('ok' in res) {
       childCanisterId = res.ok;
     }
-    childCanisterAccountId = principalToAccountIdentifier(childCanisterId.toText(), 0);
-    childCanisterActorOfAlice = createChildCanisterActorByCanisterId(childCanisterId)(identityOptionOfAlice);
+    childCanisterAccountId = principalToAccountIdentifier(
+      childCanisterId.toText(),
+      0
+    );
+    childCanisterActorOfAlice = createChildCanisterActorByCanisterId(
+      childCanisterId
+    )(identityOptionOfAlice);
 
     // Transfer the NFT to the child canister
     const childCanister: User = {
@@ -135,7 +158,9 @@ describe('Exhibit an NFT test', () => {
 
   it('Alice can exhibit the NFT to the child canister', async () => {
     const tokenIndexOnChildCanister = BigInt(1);
-    const res = await childCanisterActorOfAlice.exhibitMyNft(tokenIndexOnChildCanister);
+    const res = await childCanisterActorOfAlice.exhibitMyNft(
+      tokenIndexOnChildCanister
+    );
     expect(res).toStrictEqual({
       ok: null,
     });
@@ -148,6 +173,159 @@ describe('Exhibit an NFT test', () => {
     expect(res[0][0]).toBe(tokenIndexOnChildCanister);
     expect(res[0][1]).toStrictEqual({
       Exhibit: nft,
+    });
+  });
+});
+
+describe('Bid an NFT test', () => {
+  type TokenIndex = bigint; // TokenIndex in candid file is number, which is wrong.
+
+  let bidTokenId: TokenIdentifier;
+
+  let bidTokenIndex: TokenIndex = BigInt(1); // Assume Bob imports only one NFT
+  let exhibitTokenIndex: TokenIndex;
+  let bidTokenIndexInChildCanisterOfAlice: TokenIndex;
+
+  let bidNft: Nft;
+
+  let childCanisterIdOfAlice: CanisterID;
+  let childCanisterIdOfBob: CanisterID;
+
+  let childCanisterAccountIdOfAlice: string;
+  let childCanisterAccountIdOfBob: string;
+
+  let childCanisterActorOfAliceCalledByBob: ActorSubclass<IChildCanister>;
+  let childCanisterActorOfBob: ActorSubclass<IChildCanister>;
+
+  beforeAll(async () => {
+    // Mint an NFT
+    const mintRequest: MintRequest = {
+      to: userBob,
+      metadata: [],
+    };
+    const tokenIndex = await generativeNFTActorOfBob.mintNFT(mintRequest);
+    bidTokenId = generateTokenIdentifier(generativeNFTCanisterId, tokenIndex);
+    bidNft = { MyExtStandardNft: bidTokenId };
+
+    // Mint a child canister
+    await actorOfBob.register();
+    const res = await actorOfBob.mintChildCanister();
+    if ('ok' in res) {
+      childCanisterIdOfBob = res.ok;
+    }
+
+    childCanisterActorOfBob =
+      createChildCanisterActorByCanisterId(childCanisterIdOfBob)(
+        identityOptionOfBob
+      );
+
+    // Transfer the NFT to the child canister
+    const childCanister: User = {
+      principal: childCanisterIdOfBob,
+    };
+    const transferRequest: TransferRequest = {
+      from: userBob,
+      to: childCanister,
+      token: bidTokenId,
+      notify: false,
+      memo: [],
+      subaccount: [],
+      amount: BigInt(1),
+    };
+    await generativeNFTActorOfBob.transfer(transferRequest);
+
+    // Get Alice's child canister ids
+    const childCanisters = await actorOfBob.getAllChildCanisters();
+    childCanisterIdOfAlice = childCanisters.filter(([_, userId]) => {
+      return userId.toText() === userAlice.principal.toText();
+    })[0][0];
+    childCanisterActorOfAliceCalledByBob = createChildCanisterActorByCanisterId(
+      childCanisterIdOfAlice
+    )(identityOptionOfBob);
+
+    // Get accound id of child canisters
+    childCanisterAccountIdOfAlice = principalToAccountIdentifier(
+      childCanisterIdOfAlice.toText(),
+      0
+    );
+    childCanisterAccountIdOfBob = principalToAccountIdentifier(
+      childCanisterIdOfBob.toText(),
+      0
+    );
+  });
+
+  it('Bob can see auctions opened by Alice', async () => {
+    const res = await childCanisterActorOfAliceCalledByBob.getAuctions();
+
+    // Note that this test does not care how many auctions are open
+    // but at least one auction must be currently open.
+    expect(res.length).toBeGreaterThanOrEqual(1);
+
+    const [tokenIndex, bids] = res[0];
+    exhibitTokenIndex = tokenIndex;
+
+    expect(bids.length).toBe(0);
+  });
+
+  it("Bob's child canister can import an NFT", async () => {
+    const res = await childCanisterActorOfBob.importMyNft(bidNft);
+    expect(res).toStrictEqual({
+      ok: bidTokenIndex,
+    });
+  });
+
+  it('Make sure the child canister is the bearer of the NFT', async () => {
+    const res = await generativeNFTActorOfBob.bearer(bidTokenId);
+    expect(res).toStrictEqual({
+      ok: childCanisterAccountIdOfBob,
+    });
+  });
+
+  it('Bob can offer a bid', async () => {
+    const res = await childCanisterActorOfBob.offerBidMyNft({
+      exhibitCanisterId: childCanisterIdOfAlice.toText(),
+      bidToken: bidTokenIndex,
+      exhibitToken: exhibitTokenIndex,
+    });
+    expect(res).toStrictEqual({
+      ok: null,
+    });
+  });
+
+  it("Now auction includes a bid by Bob (technically, a bid by Bob's child canister)", async () => {
+    const res = await childCanisterActorOfAliceCalledByBob.getAuctions();
+    const [tokenIndex, bids] = res[0];
+    const [bidder, tokenIndexOfBid] = bids[0];
+    expect(tokenIndex).toBe(exhibitTokenIndex);
+    expect(bids.length).toBe(1);
+    expect(tokenIndexOfBid).toBe(bidTokenIndex);
+    expect(bidder.toText()).toBe(childCanisterIdOfBob.toText());
+  });
+
+  it("Assets in Alice's child canister include a bid by Bob", async () => {
+    const assets = await childCanisterActorOfAliceCalledByBob.getAssets();
+    const nfts = assets
+      .map((asset) => getTokenIdAndNftStatusFromAsset(asset))
+      .filter((nft) => nft.tokenId === bidTokenId);
+    expect(nfts.length).toBe(1);
+
+    bidTokenIndexInChildCanisterOfAlice = nfts[0].tokenIndexOnChildCanister;
+  });
+
+  it("On Alice's child canister, asset owner of the bidded token is Bob's child canister", async () => {
+    const res =
+      await childCanisterActorOfAliceCalledByBob.getAssetOwnerByTokenIndex(
+        bidTokenIndexInChildCanisterOfAlice
+      );
+    expect(res).toStrictEqual({
+      ok: childCanisterIdOfBob,
+    });
+  });
+
+  it("On the NFT canister, actual asset owner of the bidded token is Alice's child canister", async () => {
+    const res = await generativeNFTActorOfBob.bearer(bidTokenId);
+    expect(res).toStrictEqual({
+      ok: childCanisterAccountIdOfAlice,
     });
   });
 });
