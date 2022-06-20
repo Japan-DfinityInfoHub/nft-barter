419f649078421bf0c1d5bfdb5078f7c792824a23
hoosan <hoosanalis@gmail.com>
2022-06-20 11:41:54 +0900

    Add canister id

--- src/NFTBarter_assets/src/utils/canisterId.ts
+++ src/NFTBarter_assets/src/utils/canisterId.ts
@@ -1,9 +1,9 @@
 export const GENERATIVE_ART_NFT_CANISTER_ID =
   process.env.DFX_NETWORK === 'ic' || !process.env.LOCAL_NFT_CANISTER_ID
-    ? 'REPLACE_TO_CANISTER_ID'
+    ? 'aivvr-hqaaa-aaaap-qamjq-cai'
     : process.env.LOCAL_NFT_CANISTER_ID;
 
 export const GENERATIVE_ART_NFT_BASE_URL =
   process.env.DFX_NETWORK === 'ic' || !process.env.LOCAL_NFT_CANISTER_ID
-    ? 'http://REPLACE_TO_CANISTER_ID.ic0.app'
+    ? 'http://aivvr-hqaaa-aaaap-qamjq-cai.raw.ic0.app'
     : `http://${process.env.LOCAL_NFT_CANISTER_ID}.localhost:8000`;
