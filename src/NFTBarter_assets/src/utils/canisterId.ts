3c512180b48819d77fb351fc5f9cdc7a774dc085
hoosan <hoosanalis@gmail.com>
2022-06-20 18:24:40 +0900

    Add cycles for creating child canister

--- src/NFTBarter_assets/src/utils/canisterId.ts
+++ src/NFTBarter_assets/src/utils/canisterId.ts
@@ -5,5 +5,5 @@ export const GENERATIVE_ART_NFT_CANISTER_ID =
 
 export const GENERATIVE_ART_NFT_BASE_URL =
   process.env.DFX_NETWORK === 'ic' || !process.env.LOCAL_NFT_CANISTER_ID
-    ? 'http://aivvr-hqaaa-aaaap-qamjq-cai.raw.ic0.app'
+    ? 'https://aivvr-hqaaa-aaaap-qamjq-cai.raw.ic0.app'
     : `http://${process.env.LOCAL_NFT_CANISTER_ID}.localhost:8000`;
